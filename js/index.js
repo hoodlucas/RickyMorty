61eb9ba6092cf6e317a7f9cae53b4c90757332b5
Hector-Olivera <olivera.h.andres@gmail.com>
2026-03-17 12:19:42 -0300

    iniciando estructura

--- js/index.js
+++ js/index.js
@@ -7,8 +7,16 @@ function getCharacteres() {
         .catch(error => console.error('Error:', error));
 }
 
-function renderCharacters (arr)
-{
+function getLocations(){
+    fetch("https://rickandmortyapi.com/api/location", { method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            renderLocation(data.results);
+        })
+        .catch(error => console.log('Error:', error));
+}
+
+function renderCharacters (arr){
     for (let index = 0; index < arr.length; index++) {
                 document.querySelector(".items").innerHTML += `<div class="card" style="width: 18rem;">
                     <img src="${arr[index].image}" class="card-img-top" alt="...">
@@ -19,4 +27,18 @@ function renderCharacters (arr)
                     </div>
                     </div>`
             }
-}2
+}
+
+function renderLocation(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        document.querySelector('.locations').innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="location.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${arr[index].name}</h5>
+            <p class="card-text">${arr[index].dimension}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>`
+        
+    }
+}
