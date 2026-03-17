39f5fa1e5bea2da4c4ddd79944bfe17d0d7a40c4
Daniel Tripaylan <mateofrancogoenaga20@gmail.com>
2026-03-17 12:19:03 -0300

    Navbar, personajes y creacion de detail

--- rickymorty/js/index.js
+++ rickymorty/js/index.js
@@ -10,13 +10,18 @@ function getCharacteres() {
 function renderCharacters (arr)
 {
     for (let index = 0; index < arr.length; index++) {
-                document.querySelector(".items").innerHTML += `<div class="card" style="width: 18rem;">
-                    <img src="${arr[index].image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"> ${arr[index].name}  </h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </div>`
-            }
-}2
+        document.querySelector(".items").innerHTML += `
+            <div class="card flex-shrink-0 me-3" style="width: 18rem;">
+                <img src="${arr[index].image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${arr[index].name}</h5>
+                    <p class="card-text">${arr[index].gender}</p>
+                    <p class="card-text">${arr[index].status}</p>
+                    <p class="card-text">${arr[index].type}</p>
+                    <a href="details.html" class="btn btn-primary">More information</a>
+                </div>
+            </div>`;
+    }
+}
+
+getCharacteres()
